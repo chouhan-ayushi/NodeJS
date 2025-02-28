5a52cd48746f891b6d5d4bd15e314d4c114777ed
chouhan-ayushi <160531488+chouhan-ayushi@users.noreply.github.com>
2025-02-28 15:48:19 +0530

    Add files via upload

---
+++ Feature.js
@@ -0,0 +1,18 @@
+const http=require('http')
+
+const PORT=3022;
+const HOSTNAME ='localhost';
+
+const server=http.createServer((req, res)=>{
+   // res.statusCode=200;
+    //res.setHeader('Content-type','text/plain')
+    //res.end("Node server created by Ayushi Chouhan")
+    res.statusCode=500;
+    res.setHeader('Content-type','application/json');
+    res.end(JSON.stringify({error: "Server Error"}))
+}
+);
+server.listen(PORT ,()=>{
+    console.log("Server running at ${HOSTNAME}:{PORT}");
+}    
+)
